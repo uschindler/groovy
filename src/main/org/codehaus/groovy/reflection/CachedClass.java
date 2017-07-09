0283bf9a5b212391b9e4362d6b74939b1577a24f
Uwe Schindler <uwe@thetaphi.de>
2017-07-09 19:13:49 +0200

    Refactor the module handle to be a single one for openness check; delegate canAccess check to Java 9 API

--- src/main/org/codehaus/groovy/reflection/CachedClass.java
+++ src/main/org/codehaus/groovy/reflection/CachedClass.java
@@ -32,14 +32,11 @@ import static java.lang.invoke.MethodType.methodType;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.invoke.MethodType;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.*;
@@ -54,32 +51,36 @@ public class CachedClass {
     
     private static ReferenceBundle softBundle = ReferenceBundle.getSoftBundle();
     
-    private static final String MY_PACKAGE = CachedClass.class.getPackage().getName();
-    private static final MethodHandle MH_Class_getModule, MH_Module_isOpen;
-    private static final /* java.lang.Module */ Object GROOVY_MODULE;
+    private static final MethodHandle MH_MODULE_CHECK, MH_CAN_ACCESS;
     static {
       final Lookup lookup = MethodHandles.lookup();
-      MethodHandle mh1, mh2;
+      MethodHandle mh_check, mh_canAccess;
       try {
+        // MethodHandle to check if we can access a member without setAccessible:
+        mh_canAccess = lookup.findVirtual(AccessibleObject.class, "canAccess", methodType(boolean.class));
+        
+        /* create a pre-compiled MethodHandle that is identical to following Java 9 code:
+         *  boolean moduleCheck(Class clazz) {
+         *   Module m = clazz.getModule();
+         *   return m.isOpen(CachedClass.class.getPackage().getName(), CachedClass.class.getModule());
+         *  }
+         */
         final Class<?> moduleClass = Class.forName("java.lang.Module");
-        final MethodType mt = methodType(moduleClass);
-        mh1 = lookup.findVirtual(Class.class, "getModule", mt);
-        mh1 = mh1.asType(methodType(Object.class, Class.class));
-        mh2 = lookup.findVirtual(moduleClass, "isOpen", methodType(boolean.class, String.class, moduleClass));
-        mh2 = mh2.asType(methodType(boolean.class, Object.class, String.class, Object.class));
+        final MethodHandle mh_Class_getModule = lookup.findVirtual(Class.class, "getModule", methodType(moduleClass));
+        final MethodHandle mh_Module_isOpen = lookup.findVirtual(moduleClass, "isOpen",
+            methodType(boolean.class, String.class, moduleClass));
+        final Object module = mh_Class_getModule.invoke(CachedClass.class);
+        mh_check = MethodHandles.insertArguments(mh_Module_isOpen, 1, CachedClass.class.getPackage().getName(), module);
+        mh_check = MethodHandles.filterArguments(mh_check, 0, mh_Class_getModule);
       } catch (SecurityException/*should never happen for public methods*/ | ReflectiveOperationException e) {
-        mh1 = mh2 = null;
-      }
-      MH_Class_getModule = mh1;
-      MH_Module_isOpen = mh2;
-      
-      Object module;
-      try {
-        module = MH_Class_getModule.invokeExact(CachedClass.class);
+        mh_check = mh_canAccess = null;
+      } catch (RuntimeException | Error e) {
+        throw e;
       } catch (Throwable t) {
-        module = null;
+        throw new AssertionError(t);
       }
-      GROOVY_MODULE = module;
+      MH_MODULE_CHECK = mh_check;
+      MH_CAN_ACCESS = mh_canAccess;
     }
 
     private final LazyReference<CachedField[]> fields = new LazyReference<CachedField[]>(softBundle) {
@@ -115,23 +116,15 @@ public class CachedClass {
     };
     
     /**
-     * Returns {@code true} if the cached class is in the same Java 9 module as Groovy or if its module
-     * opens it to Groovy's module, {@code false} if it's from a foreign module (e.g., the Java runtime)
+     * Returns {@code true} if the cached class' module is open (allows reflection with {@code setAccessible()}
+     * to Groovy's own module, {@code false} if it's from a foreign module (e.g., the Java runtime)
      * and it has not opened the package.
      * For previous Java versions it always returns {@code true}.
      */
     private boolean isClassOpen() {
-        if (MH_Class_getModule != null && GROOVY_MODULE != null) {
+        if (MH_MODULE_CHECK != null) {
             try {
-                /* java.lang.Module */ Object module = (Object) MH_Class_getModule.invokeExact(getTheClass());
-                final boolean isOpen = (boolean) MH_Module_isOpen.invokeExact(module, MY_PACKAGE, GROOVY_MODULE);
-                if (isOpen) {
-                    return true;
-                }
-                // in Java 9 we cannot make anything accessible from a foreign module by default!
-                return false;
-            } catch (SecurityException se) {
-                // swallow
+                return (boolean) MH_MODULE_CHECK.invokeExact(getTheClass());
             } catch (RuntimeException | Error e) {
                 throw e;
             } catch (Throwable t) {
@@ -147,10 +140,16 @@ public class CachedClass {
             // we can only make public members available in Java 9, if the class is from a different module:
             final ArrayList<AccessibleObject> ret = new ArrayList<>(aoa.length);
             for (final AccessibleObject ao : aoa) {
-                final Member m = (Member) ao;
-                if (Modifier.isPublic(m.getModifiers())) {
-                  // no need to call setAccessible, as it's public already!
-                  ret.add(ao);
+                try {
+                    final boolean canAccess = (boolean) MH_CAN_ACCESS.invokeExact(ao);
+                    if (canAccess) {
+                      // no need to call setAccessible, as it's public already!
+                      ret.add(ao);
+                    }
+                } catch (RuntimeException | Error e) {
+                    throw e;
+                } catch (Throwable t) {
+                    throw new AssertionError(t); 
                 }
             }
             return ret.toArray((AccessibleObject[]) Array.newInstance(aoa.getClass().getComponentType(), ret.size()));
